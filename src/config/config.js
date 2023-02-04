0120838a98bf698f9aeda0875f141e08500044b2
Alejandro <alejandrovalerde@gmail.com>
2023-02-04 11:22:25 -0800

    feat: changes

--- src/config/config.js
+++ src/config/config.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: import.meta.env.API_KEY,
   authDomain: import.meta.env.AUTH_DOMAIN,
   databaseURL: import.meta.env.DATABASE_URL,
-  projectId: import.meta.env.PROYECT_ID, 
+  projectId: import.meta.env.PROJECT_ID, 
   storageBucket: import.meta.env.STORAGE_BUCKET,
   messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
   appId: import.meta.env.APP_ID 
